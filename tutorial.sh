90fe1174c7a6d2ddf57739f6eb6102545045cecc
Pavel Ivanov <zeuamsa@gmail.com>
2021-09-08 16:24:40 +0300

    and another one

--- tutorial.sh
+++ tutorial.sh
@@ -12,10 +12,10 @@ if [[ $1 == "build" ]]; then
 fi
 
 if [[ $1 == "clean" ]]; then
-    cd $SPATH/lf  && make clean
-    cd $SPATH/plf && make clean
-    cd $SPATH/qc  && make clean
-    cd $SPATH/slf && make clean
-    cd $SPATH/vc  && make clean
-    cd $SPATH/vfa && make clean
+    cd $SPATH/lf  && make clean && rm Makefile*
+    cd $SPATH/plf && make clean && rm Makefile*
+    cd $SPATH/qc  && make clean && rm Makefile*
+    cd $SPATH/slf && make clean && rm Makefile*
+    cd $SPATH/vc  && make clean && rm Makefile*
+    cd $SPATH/vfa && make clean && rm Makefile*
 fi
